03d97c794e9181c09bb5d024537e15d0aad69c58
Pedro <pedroantoniocorei15@gmail.com>
2021-04-12 23:19:23 -0500

    implementacion basica para el patron screenplay

---
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
@@ -0,0 +1,17 @@
+package co.com.choucair.certification.proyectobase.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class SearchCoursePage extends PageObject {
+
+    public static final Target BUTTON_CC=Target.the("Cursos y certificaciones")
+            .located(By.id("certificaciones"));
+    public static final Target SELECT_COURSE= Target.the("selecciona el curso")
+            .located(By.xpath("//*[@id=\"category-course-list\"]/div/div[2]/div/div[1]/div/div/a/div[2]/h4"));
+    public static final Target QUESTION= Target.the("Confirma curso")
+            .located(By.xpath("//*[@id=\"page-header\"]/div/div/div[2]/div[1]/div[1]/a/div/div/h1"));
+
+
+}
---
+++ src/test/resources/feature/academyChoucair.feature
@@ -0,0 +1,12 @@
+@stories
+Feature: Academy Choucair
+  as a user, I want to learn how to automate in screamplay at the Choucair Academy with the automation course
+  @scenario1
+
+  Scenario: Search for a automation course
+
+    Given than brandon wants to learn automation at the academy Choucair
+
+    When he search for the course Foundation Express on the choucair academy platform
+
+    Then he finds the course called resources Foundation Express
